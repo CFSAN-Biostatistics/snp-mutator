6b9062a9e1f9183488c31077d94077caf16efe8f
Steve Davis <steven.davis@fda.hhs.gov>
2015-05-06 14:04:43 -0400

    Initial commit capturing the script initial state.

---
+++ mutator/mutateGenomeArgParse.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+ 
+import argparse
+import sys
+import os.path
+from numpy.random import choice,seed
+import subprocess
+import shutil
+
+### Setup args
+usage="""Takes a fasta file and creates N base substitutions and N insertion/deletions. Assumes you have the simulation program installed in your path and outputs a summary file, mutated genomes, and fastq files form art.
+usage: %prog -i FILE (args)"""
+
+
+parser = argparse.ArgumentParser(description= usage)
+
+
+parser.add_argument("-i", "--in-file", metavar="FILE", dest="in_file", default=None,
+				help="Specify the input fasta FILE")
+
+parser.add_argument("-s", "--summary", metavar="FILE", dest="summary_file", default=None,
+				help="Specify the name of the file to output positional information to.")
+
+parser.add_argument("-n", "--number-of-simulations", metavar="INT", dest="num_sims", default=None,
+				help="Specify the number of simulations to run. Default is 100.")
+
+parser.add_argument("-m", "--number-of-substitutions", metavar="INT", dest="num_subs", default=None,
+				help="Specify the number of substitutions. Default is 500.")
+
+parser.add_argument("-t", "--num-of-insertions", metavar="INT", dest="num_insertions", default=None,
+				help="Specify the number of insertions. Default is 20.")
+
+parser.add_argument("-d", "--num-of-deletions", metavar="INT", dest="num_deletions", default=None,
+				help="Specify the number of deletions. Default is 20.")
+
+parser.add_argument("-r", "--run_snp_pipeline", metavar="STRING", dest="run_snp", default=None,
+				help="Specify whether to run snp_pipeline - yes or no. Default is No.")
+
+parser.add_argument("-e", "--random-number-seed", metavar="INT", dest="ran_seed", default=None,
+				help="Specify the random number seed - this will cause all mutated genomes to be identical. Default is to get one from the computer.")
+
+parser.add_argument("-rs", "--random-number-seed-for-art", metavar="INT", dest="art_seed", default=None,
+				help="Specify the random number seed - this will cause all fastq simulations to have the same random seed. Default is to get one from the computer.")
+
+args = parser.parse_args()
+
+
+### Input file args
+if args.in_file:
+	in_file = os.path.abspath(args.in_file)
+else:	
+	print "You must specify an in_file. Use '-h' for help."
+	sys.exit()
+
+(in_filePath, in_fileWholeName) = os.path.split(in_file)
+(in_fileBase, in_fileExt) = os.path.splitext(in_fileWholeName)
+
+###	 Summary args
+if args.summary_file:
+	contigs_file = os.path.abspath(args.summary_file)
+else:
+	summary_file = os.path.join(in_filePath, in_fileBase + "_mutatedSNPlist.txt")
+
+### Define number of simulations, substitutions and indels
+if args.num_sims == None:
+	numSims = 100
+else:
+	numSims = int(args.num_sims)
+
+if args.num_subs == None:
+	numSubs = 500
+else:
+	numSubs = int(args.num_subs)
+
+if args.num_deletions == None:
+	numDels = 20
+else:
+	numDels = int(args.num_deletions)
+
+if args.num_insertions == None:
+	numInsertions = 20
+else:
+	numInsertions = int(args.num_insertions)
+
+### Parse whether to run snp_pipeline
+if args.run_snp == None:
+	runSNP = "No"
+elif args.run_snp == "no":
+	runSNP = "No"
+elif args.run_snp == "No":
+	runSNP = "No"
+else:	
+	runSNP = args.run_snp
+
+### Parse random seed options
+if args.ran_seed == None:
+	pass
+else:
+	ranSeed = int(args.ran_seed)
+
+if args.art_seed == None:
+	pass
+else:
+	artSeed = args.art_seed
+
+### Read in reference and build dictionary - I originally used SeqIO.parse but went this route to account for a references with more than one contig
+seqString = []
+seqDict = {}
+seqName = []
+seqLength = []
+
+def getSequence(in_file):
+	with open(in_file, "r") as inFile:
+		for idx, line in enumerate(inFile):
+			if idx == 0:
+				seqName.append(line.strip(">").strip("\n"))
+			elif ">" in line:
+				pass
+			else:
+				seqString.append(line.strip("\n"))
+getSequence(in_file)
+
+def buildSeqDict(seqString):
+	seq = "".join(seqString)
+	seqLength.append(len(seq))
+	for idx, i in enumerate(seq):
+		seqDict[idx]= i
+buildSeqDict(seqString)
+
+
+### Run simulations to get mutated genome
+def runSimulations(seqDict):
+	with open(in_fileBase + "_snpListMutated.txt", "w") as snpList:
+		snpList.write("replicate\tposition\toriginalBase\tnewBase\n")
+
+		for replicate in range(0, numSims):  
+				
+### Deal with random seed and generate the positions where the numSubs will occur	
+			try:
+				seed(ranSeed)
+			except NameError:
+				pass
+				
+			try:
+				positions = choice(range(0, seqLength[0] - 1), size=int(numSubs + numDels + numInsertions), replace=False)
+			except ValueError:
+				print "ERROR: You have specified a number of substitutions that is greater than the length of the sequence"
+				sys.exit()
+			subPositions = positions[:numSubs]
+			deletionPositions = positions[numSubs:(numSubs + numDels)]
+			insertionPositions = positions[(numSubs + numDels):len(positions)]
+
+### Create a copy of the sequence dictionary and mutate sites that were identified in the previous step
+			print "Creating replicate ", str(replicate)
+
+			def buildNewSeq():
+				newSeqDict = dict(seqDict)	
+				for i in subPositions:
+					try:
+						seqDict[i]
+						state = seqDict[i]
+						if state == "A":
+							newState = str(choice(["C", "T", "G"], size = 1)[0])
+							newSeqDict[i] = newState
+							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+						if state == "C":
+							newState = str(choice(["A", "T", "G"], size = 1)[0])
+							newSeqDict[i] = newState
+							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+						if state == "T":
+							newState = str(choice(["C", "A", "G"], size = 1)[0])
+							newSeqDict[i] = newState
+							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+						if state == "G":
+							newState = str(choice(["C", "T", "A"], size = 1)[0])
+							newSeqDict[i] = newState
+							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+					except:
+						pass
+
+				for i in deletionPositions:
+					try:
+						seqDict[i]
+						state = seqDict[i]
+						newState = ""
+						newSeqDict[i] = newState
+						snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_deletion\n") 
+					except:
+						pass
+
+				for i in insertionPositions:
+					try:
+						seqDict[i]
+						state = seqDict[i]
+						newState = str(choice(["A", "C", "T", "G"], size = 1)[0]) + state
+						newSeqDict[i] = newState
+						snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_insertion\n") 
+					except:
+						pass
+				def writeNewSequence():
+					newSequence = []
+					with open(in_fileBase + "_mutated_" + str(replicate) + ".fasta", "w") as newFasta:
+						newFasta.write(">" + seqName[0] + "\n")
+						for key, value in newSeqDict.items():
+							newSequence.append(value)	
+						newFasta.write("".join(newSequence))
+				writeNewSequence()
+			buildNewSeq()
+			
+runSimulations(seqDict)
+
+
+### Run art simulations  - right now it overwrites any previous runs
+def runArtSimulations():
+	if os.path.isdir(in_filePath + "/artSims/"):
+		shutil.rmtree(in_filePath + "/artSims/")
+	else:
+		os.mkdir(in_filePath + "/artSims/")
+	for replicate in range(0, numSims):  
+		print "Running ART on replicate ", str(replicate)		
+		outDir = str(in_filePath + "/artSims/" + in_fileBase + "_mutated_" + str(replicate) + "artSims/")
+		os.makedirs(outDir)
+		inputFile = str(in_fileBase + "_mutated_" + str(replicate) + ".fasta")
+		outputFile = str(outDir + in_fileBase + "_mutated_" + str(replicate) + "artSims_")
+		try:
+			subprocess.call(["art_illumina", "-i", inputFile, "-p", "-l", "250", "-ss", "MS", "-f", "100", "-m", "500", "-s", "10", "-rs", artSeed, "-o", outputFile])
+		except NameError:
+			subprocess.call(["art_illumina", "-i", inputFile, "-p", "-l", "250", "-ss", "MS", "-f", "100", "-m", "500", "-s", "10", "-o", outputFile])
+
+runArtSimulations()
+
+
+### Run snppipeline on all replicates simulataneously
+def runSnpPipeline():
+	if runSNP == "No":
+		sys.exit()
+	else:
+		sampleDir = str(in_filePath + "/artSims/")
+		print sampleDir
+		outDir = str(in_filePath + "/" + in_fileBase + "_mutated_artSims_snpPipeline/")
+		print outDir
+		print in_file
+		subprocess.call(["run_snp_pipeline.sh", "-s", sampleDir, "-o", outDir, in_file])
+runSnpPipeline()
+
