7fd034dcf7a1d4e0d955ffc8b856aa68ce6bf6f9
Steve Davis <steven.davis@fda.hhs.gov>
2015-05-11 08:36:04 -0400

    Change tabs to spaces. Minor refactoring to facilitate testing.  This version still generated fasta files matching the original version.

--- mutator/mutator.py
+++ mutator/mutator.py
@@ -4,8 +4,6 @@ import argparse
 import sys
 import os.path
 from numpy.random import choice,seed
-import subprocess
-import shutil
 
 
 ### Read in reference and build dictionary - I originally used SeqIO.parse but went this route to account for a references with more than one contig
@@ -15,106 +13,107 @@ seqName = []
 seqLength = []
 
 def getSequence(in_file):
-	with open(in_file, "r") as inFile:
-		for idx, line in enumerate(inFile):
-			if idx == 0:
-				seqName.append(line.strip(">").strip("\n"))
-			elif ">" in line:
-				pass
-			else:
-				seqString.append(line.strip("\n"))
+    with open(in_file, "r") as inFile:
+        for idx, line in enumerate(inFile):
+            if idx == 0:
+                seqName.append(line.strip(">").strip("\n"))
+            elif ">" in line:
+                pass
+            else:
+                seqString.append(line.strip("\n"))
 
 
 def buildSeqDict(seqString):
-	seq = "".join(seqString)
-	seqLength.append(len(seq))
-	for idx, i in enumerate(seq):
-		seqDict[idx]= i
+    seq = "".join(seqString)
+    seqLength.append(len(seq))
+    for idx, i in enumerate(seq):
+        seqDict[idx]= i
 
 
 
 ### Run simulations to get mutated genome
-def runSimulations(seqDict):
-	with open(in_fileBase + "_snpListMutated.txt", "w") as snpList:
-		snpList.write("replicate\tposition\toriginalBase\tnewBase\n")
+def runSimulations(in_fileBase, numSims, numSubs, numDels, numInsertions, seqDict, ranSeed):
+    with open(in_fileBase + "_snpListMutated.txt", "w") as snpList:
+        snpList.write("replicate\tposition\toriginalBase\tnewBase\n")
 
-		for replicate in range(0, numSims):  
-				
-### Deal with random seed and generate the positions where the numSubs will occur	
-			try:
-				seed(ranSeed)
-			except NameError:
-				pass
-				
-			try:
-				positions = choice(range(0, seqLength[0] - 1), size=int(numSubs + numDels + numInsertions), replace=False)
-			except ValueError:
-				print "ERROR: You have specified a number of substitutions that is greater than the length of the sequence"
-				sys.exit()
-			subPositions = positions[:numSubs]
-			deletionPositions = positions[numSubs:(numSubs + numDels)]
-			insertionPositions = positions[(numSubs + numDels):len(positions)]
+        for replicate in range(0, numSims):  
+                
+### Deal with random seed and generate the positions where the numSubs will occur   
+            try:
+                seed(ranSeed)
+            except NameError:
+                pass
+                
+            try:
+                positions = choice(range(0, seqLength[0] - 1), size=int(numSubs + numDels + numInsertions), replace=False)
+            except ValueError:
+                print "ERROR: You have specified a number of substitutions that is greater than the length of the sequence"
+                sys.exit()
+            subPositions = positions[:numSubs]
+            deletionPositions = positions[numSubs:(numSubs + numDels)]
+            insertionPositions = positions[(numSubs + numDels):len(positions)]
 
 ### Create a copy of the sequence dictionary and mutate sites that were identified in the previous step
-			print "Creating replicate ", str(replicate)
+            print "Creating replicate ", str(replicate)
 
-			def buildNewSeq():
-				newSeqDict = dict(seqDict)	
-				for i in subPositions:
-					try:
-						seqDict[i]
-						state = seqDict[i]
-						if state == "A":
-							newState = str(choice(["C", "T", "G"], size = 1)[0])
-							newSeqDict[i] = newState
-							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
-						if state == "C":
-							newState = str(choice(["A", "T", "G"], size = 1)[0])
-							newSeqDict[i] = newState
-							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
-						if state == "T":
-							newState = str(choice(["C", "A", "G"], size = 1)[0])
-							newSeqDict[i] = newState
-							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
-						if state == "G":
-							newState = str(choice(["C", "T", "A"], size = 1)[0])
-							newSeqDict[i] = newState
-							snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
-					except:
-						pass
+            def buildNewSeq():
+                newSeqDict = dict(seqDict)  
+                for i in subPositions:
+                    try:
+                        seqDict[i]
+                        state = seqDict[i]
+                        if state == "A":
+                            newState = str(choice(["C", "T", "G"], size = 1)[0])
+                            newSeqDict[i] = newState
+                            snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+                        if state == "C":
+                            newState = str(choice(["A", "T", "G"], size = 1)[0])
+                            newSeqDict[i] = newState
+                            snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+                        if state == "T":
+                            newState = str(choice(["C", "A", "G"], size = 1)[0])
+                            newSeqDict[i] = newState
+                            snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+                        if state == "G":
+                            newState = str(choice(["C", "T", "A"], size = 1)[0])
+                            newSeqDict[i] = newState
+                            snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "\n") 
+                    except:
+                        pass
 
-				for i in deletionPositions:
-					try:
-						seqDict[i]
-						state = seqDict[i]
-						newState = ""
-						newSeqDict[i] = newState
-						snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_deletion\n") 
-					except:
-						pass
+                for i in deletionPositions:
+                    try:
+                        seqDict[i]
+                        state = seqDict[i]
+                        newState = ""
+                        newSeqDict[i] = newState
+                        snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_deletion\n") 
+                    except:
+                        pass
 
-				for i in insertionPositions:
-					try:
-						seqDict[i]
-						state = seqDict[i]
-						newState = str(choice(["A", "C", "T", "G"], size = 1)[0]) + state
-						newSeqDict[i] = newState
-						snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_insertion\n") 
-					except:
-						pass
-				def writeNewSequence():
-					newSequence = []
-					with open(in_fileBase + "_mutated_" + str(replicate) + ".fasta", "w") as newFasta:
-						newFasta.write(">" + seqName[0] + "\n")
-						for key, value in newSeqDict.items():
-							newSequence.append(value)	
-						newFasta.write("".join(newSequence))
-				writeNewSequence()
-			buildNewSeq()
-			
-
-
-if __name__ == '__main__':
+                for i in insertionPositions:
+                    try:
+                        seqDict[i]
+                        state = seqDict[i]
+                        newState = str(choice(["A", "C", "T", "G"], size = 1)[0]) + state
+                        newSeqDict[i] = newState
+                        snpList.write(str(replicate) + "\t" + str(i + 1) + "\t" + state + "\t" + newState + "_insertion\n") 
+                    except:
+                        pass
+                def writeNewSequence():
+                    newSequence = []
+                    with open(in_fileBase + "_mutated_" + str(replicate) + ".fasta", "w") as newFasta:
+                        newFasta.write(">" + seqName[0] + "\n")
+                        for key, value in newSeqDict.items():
+                            newSequence.append(value)   
+                        newFasta.write("".join(newSequence))
+                writeNewSequence()
+            buildNewSeq()
+            
+            
+def parse_arguments(system_args):
+    """
+    """
     usage = """Generate mutated sequence files from a reference genome.  Takes a fasta file and creates 
                a specified number of randomly generated base substitutions, insertions, and deletions.  
                Outputs the mutated genomes, and optionally, a summary file listing the mutations by 
@@ -131,7 +130,12 @@ if __name__ == '__main__':
     parser.add_argument("-r", "--random-seed",       metavar="INT",  dest="random_seed",      type=int, default=None, help="Random number seed making the results reproducible.")
     
     args = parser.parse_args()
-    
+    return args
+            
+
+def main(args):
+    """
+    """
     # Input file arg
     in_file = args.input_fasta_file
     in_filePath, in_fileWholeName = os.path.split(in_file)
@@ -153,5 +157,11 @@ if __name__ == '__main__':
     # Read the reference and generate mutations
     getSequence(in_file)
     buildSeqDict(seqString)
-    runSimulations(seqDict)
+    runSimulations(in_fileBase, numSims, numSubs, numDels, numInsertions, seqDict, ranSeed)
+
+
+if __name__ == '__main__':
+    args = parse_arguments(sys.argv)
+    main(args)
+    
 
